290bf2d0e00b9f4c6132da2af78b6144e131a8e3
YYE2022 <114246591+YYE2022@users.noreply.github.com>
2022-12-09 11:24:27 -0500

    Update click.js

--- js/click.js
+++ js/click.js
@@ -1,3 +1,4 @@
-<script>document.getElementById("demo").addEventListener("click", myFunction);
+<script>
+document.getElementById("demo").addEventListener("click", myFunction);
    function myFunction() {
      document.getElementById("demo").innerHTML = "Please give us a call at +1(123)456-7890 for more details :) ";}</script>
