d7fb7a447bcff78a96977e2f3244464f8420b534
YYE2022 <yingjia.ye@flemingcollege.ca>
2022-12-09 12:30:25 -0500

    js edit

--- js/click.js
+++ js/click.js
@@ -1,3 +1,3 @@
-document.getElementById("demo").addEventListener("click", myFunction);
-   function myFunction() {
-     document.getElementById("demo").innerHTML = "Please give us a call at +1(123)456-7890 for more details :) ";}
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Please give us a call at +1(123)456-7890 for more details :) ";
+}
